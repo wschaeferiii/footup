44902cbb3681181dd6c4a96a0e1302643a83fd20
Will Schaefer III <wschaeferiii@gmail.com>
2016-04-28 13:00:37 -0400

    commented out my_games migration file

--- db/migrate/20160426121055_create_my_games.rb
+++ db/migrate/20160426121055_create_my_games.rb
@@ -1,8 +1,8 @@
 class CreateMyGames < ActiveRecord::Migration
   def change
     create_table :my_games do |t|
-      t.user_id :integer
-      t.game_id :integer
+      # t.user_id :integer
+      # t.game_id :integer
 
       t.timestamps null: false
     end
