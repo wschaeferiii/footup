ffb5ecb401f979584742c5ad4691c13b5bc6b724
Will Schaefer III <wschaeferiii@gmail.com>
2016-04-27 22:54:32 -0400

    created custom views for devise, added a name field to the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :games
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match 'games/:id/toggle_joined', to: 'games#toggle_joined', via: 'get'
+  devise_for :users, controllers: { registrations: "registrations" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
